6372291f05a79215a39e3a2713b25e7cf810cfab
Caio Augusto Silva <caiosilva50@gmail.com>
2020-10-22 21:58:15 -0300

    Implement publish function in easymqtt

---
+++ easymqtt/publish.php
@@ -0,0 +1,30 @@
+<?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+if(!isset($_GET['session']) || empty($_GET['session']) || !isset($_GET['topic']) || empty($_GET['topic']) || !isset($_GET['value']) || empty($_GET['value']))
+{
+    echo(json_encode(array("success" => False, "result" => "Invalid Parameters")));
+    die();
+}
+
+$session = htmlspecialchars($_GET["session"]);
+$topic = htmlspecialchars($_GET["topic"]);
+$value = htmlspecialchars($_GET["value"]);
+
+if (!is_numeric($value)){
+	$return = array("success" => False, "result" => "Error publishing value '".$value."'' to topic '".$topic."'. Non-numeric input value!");
+
+}else{
+	$ret;
+	$out;
+
+	$err=exec("python3 server/publish.py ".$session."/".$topic." ".$value,$out,$ret);
+	if ($ret == 0)
+		$return = array("success" => True, "result" => "Value '".$value."'' published to topic '".$topic."'' sucessfully!");
+	else
+		$return = array("success" => False, "result" => "Error publishing value '".$value."'' to topic '".$topic."'. ".$err);
+}
+
+echo(json_encode($return));
+
+?>
---
+++ easymqtt/server/publish.py
@@ -0,0 +1,14 @@
+import paho.mqtt.client as mqtt
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: publish.py topic value")
+    exit(1)
+
+
+mqtt_client = mqtt.Client()
+mqtt_client.username_pw_set("bipes", password="m8YLUr5uW3T")
+mqtt_client.connect("bipes.net.br", 1883, 60)
+mqtt_client.publish(sys.argv[1], sys.argv[2]);
+mqtt_client.disconnect();
+exit(0)
