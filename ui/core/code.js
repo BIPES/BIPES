9a87ca0f445bf178af65dffea119323a173cd5d2
Matheus <matheus.mr.trabalho@gmail.com>
2021-08-27 12:13:35 -0300

    Mudando idioma inicial

--- ui/core/code.js
+++ ui/core/code.js
@@ -57,7 +57,7 @@ Code.getLang = function() {
   var lang = Code.getStringParamFromUrl('lang', '');
   if (Code.LANGUAGE_NAME[lang] === undefined) {
     // Default to English.
-    lang = 'en';
+    lang = 'pt-br';
   }
   return lang;
 };
