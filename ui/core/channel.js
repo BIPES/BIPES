51fc66e145245de002d2e5c01be22b8483e502a8
Matheus <matheus.mr.trabalho@gmail.com>
2021-08-27 12:12:47 -0300

    Corrigindo links

--- ui/core/channel.js
+++ ui/core/channel.js
@@ -29,9 +29,9 @@ class mux {
     }
 
     this.ifunavailable = {
-      webserial: ['https://bipes.net.br/beta2/ui', 'the HTTPS version'],
+      webserial: ['https://iot-pete.ddns.net', 'the HTTPS version'],
       websocket: ['http://iot-pete.ddns.net', 'the HTTP version'],
-      webbluetooth: ['https://bipes.net.br/beta2/ui', 'the HTTPS version']
+      webbluetooth: ['https://iot-pete.ddns.net', 'the HTTPS version']
     }
   }
 	/**
