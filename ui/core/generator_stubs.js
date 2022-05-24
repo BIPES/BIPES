a156a17e41d50b7fc7e996f503cf9ed6c3e1fb4c
jet <jetannenbaum@gmail.com>
2022-05-24 19:55:32 -0400

    Fixed loading HCSR04 library

--- ui/core/generator_stubs.js
+++ ui/core/generator_stubs.js
@@ -4708,42 +4708,7 @@ Blockly.Python['hcsr_init'] = function(block) {
 
   Blockly.Python.definitions_['import_time'] = 'import time';
   Blockly.Python.definitions_['import_pin'] = 'from machine import Pin';
-  Blockly.Python.definitions_['import_hcr'] = `
-
-from machine import time_pulse_us
-
-#Based on: https://github.com/lemariva/uPySensors
-class HCSR04:
-    def __init__(self, trigger_pin, echo_pin, echo_timeout_us=500*2*30):
-        self.echo_timeout_us = echo_timeout_us
-        self.trigger = Pin(trigger_pin, mode=Pin.OUT)
-        self.trigger.value(0)
-        self.echo = Pin(echo_pin, mode=Pin.IN)
-
-    def _send_pulse_and_wait(self):
-        self.trigger.value(0)
-        time.sleep_us(5)
-        self.trigger.value(1)
-        time.sleep_us(10)
-        self.trigger.value(0)
-        try:
-			pulse_time = time_pulse_us(self.echo, 1, self.echo_timeout_us)
-			return pulse_time
-        except OSError as ex:
-            if ex.args[0] == 110: # 110 = ETIMEDOUT
-                raise OSError('Out of range')
-            raise ex
-
-    def distance_mm(self):
-        pulse_time = self._send_pulse_and_wait()
-        mm = pulse_time * 100 // 582
-        return mm
-
-    def distance_cm(self):
-        pulse_time = self._send_pulse_and_wait()
-        cms = (pulse_time / 2) / 29.1
-        return cms
-`;
+  Blockly.Python.definitions_['import_hcr'] = 'from hcsr04 import HCSR04';
 
 /*
  * while 1:
@@ -4753,8 +4718,6 @@ class HCSR04:
 	time.sleep(1)
 
 */
-
-
   var code = 'ultraSoundSensor = HCSR04(trigger_pin=' + pTrig + ', echo_pin=' + pEcho + ', echo_timeout_us=' + pTime + ')\n';
 
   return code;
